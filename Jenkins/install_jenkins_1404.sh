01518b96dbbd1c2ce9dc1be3a9a26510aec32838
Matt McSpirit <mattmc@outlook.com>
2016-12-20 13:14:51 -0800

    Update and rename install_jenkins.sh to install_jenkins_1404.sh

--- Jenkins/install_jenkins_1404.sh
+++ Jenkins/install_jenkins_1404.sh
@@ -3,13 +3,13 @@
 # Install Jenkins Dependencies
 apt-get update
 apt-get upgrade -y
-apt-get install openjdk-8-jdk -y
-apt-get install openjdk-8-jre -y
+apt-get install openjdk-7-jdk -y
+apt-get install openjdk-7-jre -y
 apt-get install wget -y
 
 # Set Java environment variables
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
 
 # Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
