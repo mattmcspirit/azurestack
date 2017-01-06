4ceeb6075ea2b83b2abd4baf19ac68916e0f3303
Matt McSpirit <mattmc@outlook.com>
2017-01-06 15:50:18 -0800

    Updated with JDK8/JRE8

--- mrp/install_mrp_dependencies_1404.sh
+++ mrp/install_mrp_dependencies_1404.sh
@@ -3,12 +3,14 @@
 # Install PartsUnlimitedMRP dependencies
 apt-get update
 apt-get upgrade -y
-apt-get install openjdk-7-jdk -y
-apt-get install openjdk-7-jre -y
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get install openjdk-8-jdk -y
+apt-get install openjdk-8-jre -y
 apt-get install mongodb -y
 apt-get install tomcat7 -y
 apt-get install wget -y
 
 # Set Java environment variables
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
