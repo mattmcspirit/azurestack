a974f5475118638dbf1d835afb5c67042fc0a595
Matt McSpirit <mattmc@outlook.com>
2017-06-29 12:24:58 -0700

    Update install_salt.sh

--- scripts/install_salt.sh
+++ scripts/install_salt.sh
@@ -5,7 +5,7 @@ if [[ !("$#" -eq 1) ]];
     then echo "Parameters missing for Salt Master configuration." >&2
     exit 1
 fi
-
+hostnam,e 
 # Get parameters and assign variables
 salt_fqdn=$1
 
@@ -14,7 +14,6 @@ sudo hostname "${salt_fqdn}"
 
 # Get latest updates and install wget
 apt-get update
-apt-get install wget -y
 
 # Import the SaltStack Repo Key
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
@@ -25,6 +24,9 @@ echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >
 # Get latest updates & install the Salt Master and Minion
 apt-get update
 apt-get install salt-master salt-minion -y
+
+# Open Firewall
+sudo ufw allow 4505:4506/tcp
 
 # Create Configuration Management Directory Structure
 sudo mkdir -p /srv/{salt,formulas,pillar}
@@ -50,6 +52,7 @@ EOF
 # Edit Salt Minion Configuration File
 cat << EOF > /etc/salt/minion
 master: $(hostname)
+hash_type: sha512
 master_finger: $(salt-key -F | grep master.pub | awk '{print $2}')
 EOF
 
