70461df4b3a4eb4469168659fe495a49de0b8e84
Matt McSpirit <mattmc@outlook.com>
2017-08-23 14:59:39 -0700

    Update install_puppet.sh

--- scripts/install_puppet.sh
+++ scripts/install_puppet.sh
@@ -1,11 +1,27 @@
+#!/bin/bash
+# Add the Puppet Repo with latest bits
 wget https://apt.puppetlabs.com/puppet-release-xenial.deb
 ls -ltrh puppet-release-xenial.deb 
 sudo dpkg -i puppet-release-xenial.deb 
+
+# Get latest updates
 sudo apt-get update
+
+# View cache & install Puppet Server
 sudo apt-cache show puppetserver
 sudo apt-get install puppetserver -y
+
+# Check Puppet Server Status
 systemctl status puppetserver
+
+# Start Puppet Server Service
 systemctl start puppetserver
+
+# Re-Check Puppet Server Status
 systemctl status puppetserver
+
+# Update Firewall iptables rules
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8140 -j ACCEPT
+
+#Sync with Puppet Agent
 puppet agent -t
