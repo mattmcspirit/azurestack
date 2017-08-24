3c95e2f8025115f7cb8ef9d0f36f7e21abaf2dfa
Matt McSpirit <mattmc@outlook.com>
2017-08-24 15:00:57 -0700

    Update install_puppet.sh

--- scripts/install_puppet.sh
+++ scripts/install_puppet.sh
@@ -2,7 +2,6 @@
 
 # Add the Puppet Repo with latest bits
 wget https://apt.puppetlabs.com/puppet-release-xenial.deb
-# ls -ltrh puppet-release-xenial.deb 
 sudo dpkg -i puppet-release-xenial.deb 
 
 # Get latest updates
@@ -10,16 +9,21 @@ sudo apt-get update
 
 # View cache & install Puppet Server
 sudo apt-cache show puppetserver
-DEBIAN_FRONTEND=noninteractive && apt-get install puppetserver -y
+sudo DEBIAN_FRONTEND=noninteractive && apt-get install puppetserver -y
 
 # Check Puppet Server Status
-systemctl status puppetserver --no-pager
+sudo systemctl status puppetserver --no-pager
 
-# Start Puppet Server Service
-systemctl start puppetserver
+# Update puppet.conf file to reflect hostname
+/opt/puppetlabs/bin/puppet config set --section master dns_alt_names $(hostname -f),$(hostname)
+/opt/puppetlabs/bin/puppet config set --section main certname $(hostname -f)
+/opt/puppetlabs/bin/puppet config set --section main server $(hostname -f)
+/opt/puppetlabs/bin/puppet config set --section main environment production
+/opt/puppetlabs/bin/puppet config set --section main runinterval 1h
 
-# Re-Check Puppet Server Status
-systemctl status puppetserver --no-pager
+# Start puppetmaster service
+sudo systemctl start puppetserver
+sudo systemctl enable puppetserver
 
 # Update Firewall iptables rules
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8140 -j ACCEPT
@@ -27,25 +31,8 @@ iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8140 -j ACCEPT
 # Enable the Puppet Agent on the Puppet Master
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 
-# Update the executable location path for Puppet for future reboots
-# echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.profile
-# echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.bashrc
-# export PATH=/opt/puppetlabs/bin:$PATH
-
-# Update the hostname for the agent
-sudo /opt/puppetlabs/bin/puppet config set --section agent server $(hostname)
-
-# Update certificates for agents
-# rm /etc/puppetlabs/puppet/ssl/private_keys/$(hostname -f).pem
-# rm /etc/puppetlabs/puppet/ssl/ca/signed/$(hostname -f).pem
-# rm /etc/puppetlabs/puppet/ssl/certs/$(hostname -f).pem
-# sudo /opt/puppetlabs/bin/puppet cert generate $(hostname -f) --dns_alt_names=$(hostname -f),$(hostname)
-
-# Restart Puppet Master
-# service puppetserver restart
-
-#Wait for the service to start and initial config to sync
-# sleep 30
+# Wait for the service to start and initial config to sync
+sleep 30
 
 # Sync Puppet Agent and Master
-# sudo /opt/puppetlabs/bin/puppet agent --test
+sudo /opt/puppetlabs/bin/puppet agent --test
