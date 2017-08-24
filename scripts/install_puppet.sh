2c0e3c2621ee7a4098edce275e319f3902b8b29d
Matt McSpirit <mattmc@outlook.com>
2017-08-24 14:12:32 -0700

    Update install_puppet.sh

--- scripts/install_puppet.sh
+++ scripts/install_puppet.sh
@@ -28,9 +28,9 @@ iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8140 -j ACCEPT
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 
 # Update the executable location path for Puppet for future reboots
-echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.profile
-echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.bashrc
-export PATH=/opt/puppetlabs/bin:$PATH
+# echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.profile
+# echo 'export PATH=/opt/puppetlabs/bin:$PATH' >> ~/.bashrc
+# export PATH=/opt/puppetlabs/bin:$PATH
 
 # Update the hostname for the agent
 sudo /opt/puppetlabs/bin/puppet config set --section agent server $(hostname)
