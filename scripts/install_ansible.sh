aca4f413500c644cf6b1638520f1c34505e4559d
Matt McSpirit <mattmc@outlook.com>
2017-08-28 14:42:34 -0700

    Update install_ansible.sh

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,13 +1,15 @@
-%#!/bin/bash
+ -y%#!/bin/bash
 
 # Add the Ansible PPA Repo
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 
 # Get latest updates
 sudo apt-get update
 
 # Install Ansible
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 
 # Update profiles to reflect Terraform PATH and source it
 echo 'export PATH=$PATH:~/terraform/' >> ~/.bashrc
