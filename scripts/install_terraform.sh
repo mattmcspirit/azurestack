4edd7c90a940124a28fe2e71cc3fc02986fff32f
Matt McSpirit <mattmc@outlook.com>
2017-08-28 14:33:57 -0700

    Update install_terraform.sh

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -15,14 +15,14 @@ sudo apt-get update
 sudo apt-get install jq unzip -y
 
 # Create directories to house Terraform files
-mkdir -p /opt/terraform && cd $_
+cd
+mkdir terraform && cd $_
 
 # Download the latest version of x64 Terraform and unzip
 terraform_url=$(curl https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.amd64" | sort --version-sort -r | head -1 | awk -F[\"] '{print $4}')
 curl -o terraform.zip $terraform_url
-unzip -o terraform.zip
+unzip terraform.zip
 
 # Update profiles to reflect Terraform PATH and source it
-echo 'export PATH=$PATH:~/opt/terraform/' >> ~/.bashrc
-echo 'export PATH=$PATH:~/opt/terraform/' >> /home/${adminUsername}/.profile
-source ~/.bashrc
+echo 'export PATH=$PATH:~/terraform/' >> ~/.bashrc
+echo 'export PATH=$PATH:~/terraform/' >> /home/${adminUsername}/.profile
