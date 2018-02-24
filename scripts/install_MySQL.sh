8956a137508bf8a220eda7a11318caa87cf91462
Matt McSpirit <mattmc@outlook.com>
2018-02-23 19:20:18 -0800

    Updated MySQL Install Script

--- scripts/install_MySQL.sh
+++ scripts/install_MySQL.sh
@@ -11,20 +11,20 @@ fi
 MySQLPassword=$1
 
 # Download and Install the Latest Updates for the OS
-#sudo apt-get update -y
+sudo apt-get update -y
 
 # Enable Ubuntu Firewall and allow SSH & MySQL Ports
-#ufw --force enable
-#ufw allow 22
-#ufw allow 3306
+ufw --force enable
+ufw allow 22
+ufw allow 3306
 
 # Install MySQL 5.7
-#echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
-#echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
-#apt-get -y install mysql-server-5.7 mysql-client
+echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.7 mysql-client
 
-# Reset MySQL Password & grant remote connectivity permissions
-#mysql -u root -proot -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$1') WHERE User='root'; flush privileges;"
+# Reset MySQL Password to match supplied parameter
+mysql -u root -proot -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$1') WHERE User='root'; flush privileges;"
 
 # Optionally allow remote connectivity for the root user
 # mysql -u root -p$1 -e "use mysql; CREATE USER 'root'@'%' IDENTIFIED BY '$1'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$1'; flush privileges;"
