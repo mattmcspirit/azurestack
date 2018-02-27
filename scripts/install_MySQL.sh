842b65c6aa5bfee3bd1f05a226fa0156b3153535
Matt McSpirit <mattmc@outlook.com>
2018-02-27 15:11:35 -0800

    Adding MySQL Packages

---
+++ scripts/install_MySQL.sh
@@ -0,0 +1,30 @@
+
+%#!/bin/bash
+
+# Validate input parameters
+if [[ !("$#" -eq 1) ]]; 
+    then echo "Parameters missing for MySQL configuration." >&2
+    exit 1
+fi
+
+# Get parameters and assign variables
+MySQLPassword=$1
+
+# Download and Install the Latest Updates for the OS
+sudo apt-get update -y
+
+# Enable Ubuntu Firewall and allow SSH & MySQL Ports
+ufw --force enable
+ufw allow 22
+ufw allow 3306
+
+# Install MySQL 5.7
+echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.7 mysql-client
+
+# Reset MySQL Password to match supplied parameter
+mysql -u root -proot -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$1') WHERE User='root'; flush privileges;"
+
+# Optionally allow remote connectivity for the root user
+# mysql -u root -p$1 -e "use mysql; CREATE USER 'root'@'%' IDENTIFIED BY '$1'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$1'; flush privileges;"
