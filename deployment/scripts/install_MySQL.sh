8bed52cb421b33c1a472ebaa9b67a3d9f2166f07
Matt McSpirit <mattmc@outlook.com>
2018-04-04 20:39:05 -0700

    Edited for parameter config

--- deployment/scripts/install_MySQL.sh
+++ deployment/scripts/install_MySQL.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Validate input parameters
-if [[ !("$#" -eq 2) ]]; 
-    then echo "Parameters missing for MySQL configuration." >&2
+if [[ ! ("$#" -eq 2) ]]; 
+then
+    echo "Parameters missing for MySQL configuration." >&2
     exit 1
 fi
 
 # Get parameters and assign variables
 MySQLPassword=$1
-AllowRemoteConnections=$2
+AllowRemoteConnections=$(echo "$2" | tr '[:upper:]' '[:lower:]')
 
 # Download and Install the Latest Updates for the OS
 sudo apt-get update -y
@@ -24,12 +25,13 @@ echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo de
 apt-get -y install mysql-server-5.7 mysql-client
 
 # Reset MySQL Password to match supplied parameter
-mysql -u root -proot -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$1') WHERE User='root'; flush privileges;"
+mysql -u root -proot -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$MySQLPassword') WHERE User='root'; flush privileges;"
 
-if [$2 -eq "Yes"] then
+if [ "$AllowRemoteConnections" = "yes" ]
+then
     echo "Setting up remote connections for root user"
     # Allow remote connectivity for the root user
-    mysql -u root -p$1 -e "use mysql; CREATE USER 'root'@'%' IDENTIFIED BY '$1'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$1'; flush privileges;"
+    mysql -u root -p"$MySQLPassword" -e "use mysql; CREATE USER 'root'@'%' IDENTIFIED BY '$MySQLPassword'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MySQLPassword'; flush privileges;"
     # Edit the MySQL Configuration File to allow Remote Connectivity
     sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
