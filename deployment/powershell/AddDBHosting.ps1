ddbb19666b0a9d4ceb445701dd2c666629242e8c
Matt McSpirit <mattmc@outlook.com>
2018-10-27 11:55:01 -0700

    Updated with DB model

--- deployment/powershell/AddDBHosting.ps1
+++ deployment/powershell/AddDBHosting.ps1
@@ -60,53 +60,44 @@ $runTime = $(Get-Date).ToString("MMdd-HHmmss")
 $fullLogPath = "$logPath\$($logName)$runTime.txt"
 Start-Transcript -Path "$fullLogPath" -Append -IncludeInvocationHeader
 
-$progress = Import-Csv -Path $ConfigASDKProgressLogPath
-$RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
+$progressStage = $progressName
+$progressCheck = CheckProgress -progressStage $progressStage
 
-if ($progress[$RowIndex].Status -eq "Complete") {
-    Write-Verbose -Message "ASDK Configuration Stage: $($progress[$RowIndex].Stage) previously completed successfully"
+if ($progressCheck -eq "Complete") {
+    Write-Verbose -Message "ASDK Configuration Stage: $progressStage previously completed successfully"
 }
-elseif (($skipRP -eq $false) -and ($progress[$RowIndex].Status -ne "Complete")) {
+elseif (($skipRP -eq $false) -and ($progressCheck -ne "Complete")) {
     # We first need to check if in a previous run, this section was skipped, but now, the user wants to add this, so we need to reset the progress.
-    if ($progress[$RowIndex].Status -eq "Skipped") {
+    if ($progressCheck -eq "Skipped") {
         Write-Verbose -Message "Operator previously skipped this step, but now wants to perform this step. Updating ConfigASDKProgressLog.csv file to Incomplete."
-        # Update the ConfigASDKProgressLog.csv file with successful completion
-        $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-        $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
-        $progress[$RowIndex].Status = "Incomplete"
-        $progress | Export-Csv $ConfigASDKProgressLogPath -NoTypeInformation -Force
-        $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
+        # Update the ConfigASDKProgressLog.csv file back to incomplete
+        StageReset -progressStage $progressStage
+        $progressCheck = CheckProgress -progressStage $progressStage
     }
-    if (($progress[$RowIndex].Status -eq "Incomplete") -or ($progress[$RowIndex].Status -eq "Failed")) {
+    if (($progressCheck -eq "Incomplete") -or ($progressCheck -eq "Failed")) {
         try {
-            if ($progress[$RowIndex].Status -eq "Failed") {
+            if ($progressCheck -eq "Failed") {
                 # Update the ConfigASDKProgressLog.csv file back to incomplete status if previously failed
-                $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-                $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
-                $progress[$RowIndex].Status = "Incomplete"
-                $progress | Export-Csv $ConfigASDKProgressLogPath -NoTypeInformation -Force
+                StageReset -progressStage $progressStage
+                $progressCheck = CheckProgress -progressStage $progressStage
             }
             # Need to ensure this stage doesn't start before the database SKU has been added
-            $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-            $dbSkuJobCheck = [array]::IndexOf($progress.Stage, "$($dbHost)SKUQuota")
-            while (($progress[$dbSkuJobCheck].Status -ne "Complete")) {
+            $dbSkuJobCheck = $progressCheck = CheckProgress -progressStage "$($dbHost)SKUQuota"
+            while ($dbSkuJobCheck -ne "Complete") {
                 Write-Verbose -Message "The $($dbHost)SKUQuota stage of the process has not yet completed. Checking again in 20 seconds"
                 Start-Sleep -Seconds 20
-                $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-                $dbSkuJobCheck = [array]::IndexOf($progress.Stage, "$($dbHost)SKUQuota")
-                if ($progress[$dbSkuJobCheck].Status -eq "Failed") {
+                $dbSkuJobCheck = $progressCheck = CheckProgress -progressStage "$($dbHost)SKUQuota"
+                if ($dbSkuJobCheck -eq "Failed") {
                     throw "The $($dbHost)SKUQuota stage of the process has failed. This should fully complete before the $dbHost database host has been deployed. Check the $($dbHost)SKUQuota log, ensure that step is completed first, and rerun."
                 }
             }
             # Need to ensure this stage doesn't start before the database host has finished deployment
-            $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-            $dbHostJobCheck = [array]::IndexOf($progress.Stage, "$($dbHost)DBVM")
-            while (($progress[$dbHostJobCheck].Status -ne "Complete")) {
+            $dbHostJobCheck = CheckProgress -progressStage "$($dbHost)DBVM"
+            while ($dbHostJobCheck -ne "Complete") {
                 Write-Verbose -Message "The $($dbHost)DBVM stage of the process has not yet completed. Checking again in 20 seconds"
                 Start-Sleep -Seconds 20
-                $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-                $dbHostJobCheck = [array]::IndexOf($progress.Stage, "$($dbHost)DBVM")
-                if ($progress[$dbHostJobCheck].Status -eq "Failed") {
+                $dbHostJobCheck = CheckProgress -progressStage "$($dbHost)DBVM"
+                if ($dbHostJobCheck -eq "Failed") {
                     throw "The $($dbHost)DBVM stage of the process has failed. This should fully complete before the $dbHost database host has been deployed. Check the $($dbHost)DBVM log, ensure that step is completed first, and rerun."
                 }
             }
@@ -127,14 +118,12 @@ elseif (($skipRP -eq $false) -and ($progress[$RowIndex].Status -ne "Complete")) 
                 $dbFqdn = (Get-AzureRmPublicIpAddress -Name "sql_ip" -ResourceGroupName $dbrg).DnsSettings.Fqdn
             }
             # Need to ensure this stage doesn't start before the Ubuntu Server images have been put into the PIR
-            $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-            $addHostingJobCheck = [array]::IndexOf($progress.Stage, "$hostingJobCheck")
-            while (($progress[$addHostingJobCheck].Status -ne "Complete")) {
+            $addHostingJobCheck = CheckProgress -progressStage "$hostingJobCheck"
+            while ($addHostingJobCheck -ne "Complete") {
                 Write-Verbose -Message "The $hostingJobCheck stage of the process has not yet completed. Checking again in 20 seconds"
                 Start-Sleep -Seconds 20
-                $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-                $addHostingJobCheck = [array]::IndexOf($progress.Stage, "$hostingJobCheck")
-                if ($progress[$addHostingJobCheck].Status -eq "Failed") {
+                $addHostingJobCheck = CheckProgress -progressStage "$hostingJobCheck"
+                if ($addHostingJobCheck -eq "Failed") {
                     throw "The $hostingJobCheck stage of the process has failed. This should fully complete before the database VMs can be deployed. Check the $hostingJobCheck log, ensure that step is completed first, and rerun."
                 }
             }
@@ -157,34 +146,21 @@ elseif (($skipRP -eq $false) -and ($progress[$RowIndex].Status -ne "Complete")) 
                     -skuName "MSSQL2017" -Mode Incremental -Verbose -ErrorAction Stop
             }
             # Update the ConfigASDKProgressLog.csv file with successful completion
-            Write-Verbose "Updating ConfigASDKProgressLog.csv file with successful completion`r`n"
-            $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-            $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
-            $progress[$RowIndex].Status = "Complete"
-            $progress | Export-Csv $ConfigASDKProgressLogPath -NoTypeInformation -Force
-            Write-Output $progress | Out-Host
+            $progressStage = $progressName
+            StageComplete -progressStage $progressStage
         }
         catch {
-            $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-            $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
-            $progress[$RowIndex].Status = "Failed"
-            $progress | Export-Csv $ConfigASDKProgressLogPath -NoTypeInformation -Force
-            Write-Output $progress | Out-Host
+            StageFailed -progressStage $progressStage
             Set-Location $ScriptLocation
-            Write-Verbose "ASDK Configuration Stage: $($progress[$RowIndex].Stage) Failed`r`n"
             throw $_.Exception.Message
             return
         }
     }
 }
-elseif (($skipRP) -and ($progress[$RowIndex].Status -ne "Complete")) {
-    Write-Verbose -Message "Operator chose to skip Resource Provider Deployment`r`n"
-    # Update the ConfigASDKProgressLog.csv file with successful completion
-    $progress = Import-Csv -Path $ConfigASDKProgressLogPath
-    $RowIndex = [array]::IndexOf($progress.Stage, "$progressName")
-    $progress[$RowIndex].Status = "Skipped"
-    $progress | Export-Csv $ConfigASDKProgressLogPath -NoTypeInformation -Force
-    Write-Output $progress | Out-Host
+elseif (($skipRP) -and ($progressCheck -ne "Complete")) {
+    # Update the ConfigASDKProgressLog.csv file with skip status
+    $progressStage = $progressName
+    StageSkipped -progressStage $progressStage
 }
 Set-Location $ScriptLocation
 Stop-Transcript -ErrorAction SilentlyContinue
